dcb9759903c0ab296521e736056547120480f13b
Dobashi, Hiroki <hiroki.dobashi@gmail.com>
2022-02-11 02:40:12 +0900

    Add conditions to urlize

--- src/main/helpers/string.ts
+++ src/main/helpers/string.ts
@@ -1,6 +1,11 @@
 import kebabCase from "lodash.kebabcase";
 
 export const urlize = (string: string): string => {
+  // returns the same value as the input when it's a perfect
+  // string consisting only of alphanumericals and slashes
+  if (string.match(/^[a-z\d\-\/]+$/)) {
+    return string;
+  }
   const kebabStrings = string.split("/").map((str) => kebabCase(str));
   const kebab = kebabStrings.join("/");
   const startWithSlash = new RegExp(/^\/[^ ].+/);
